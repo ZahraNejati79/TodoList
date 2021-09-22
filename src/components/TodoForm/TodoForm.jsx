79483706ebb14dc34c7936b9f02cd84bbf977e11
ZahraNejati <nejatizahra4@gmail.com>
2021-09-22 07:58:09 +0330

    Initial changes

---
+++ src/components/TodoForm/TodoForm.jsx
@@ -0,0 +1,12 @@
+const TodoForm = () => {
+  return (
+    <div className="container">
+      <form>
+        <input type="text" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
